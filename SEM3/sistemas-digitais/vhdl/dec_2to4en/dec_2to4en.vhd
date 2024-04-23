49536aac0cd20a83fc74325687266c79c04754cf
Eduardo Fiorentin <95770285+EduardoFiorentin@users.noreply.github.com>
2024-04-23 20:51:46 -0300

    upd

--- SEM3/sistemas-digitais/vhdl/dec_2to4en/dec_2to4en.vhd
+++ SEM3/sistemas-digitais/vhdl/dec_2to4en/dec_2to4en.vhd
@@ -9,23 +9,57 @@ entity decoder_2to4en is
     );
 end entity decoder_2to4en;
 
-architecture behav_decoder_2to4en of decoder_2to4en is
-begin
+-- if 
+-- architecture behav_decoder_2to4en_if of decoder_2to4en is
+-- begin
+
+--     process(A, EN)
+--     begin
+
+--         if EN = '0' then Y_L <= "0000";
+--         else 
+
+--             if A = "00" then Y_L <= "0001"; 
+--             elsif A = "01" then Y_L <= "0010";
+--             elsif A = "10" then Y_L <= "0100";
+--             elsif A = "11" then Y_L <= "1000";
+--             else Y_L <= "0000";
+--             end if;
+
+--         end if;
+
+--     end process; 
+-- end behav_decoder_2to4en_if;
+
+-- case
+architecture behav_decoder_2to4en of decoder_2to4en is 
+begin 
 
     process(A, EN)
     begin
 
-        if EN = '0' then Y_L <= "0000";
-        else 
+        Y_L <= "0000"; 
+        
+        if EN = '1' then 
+            case A is
+                when "00" =>
+                    Y_L <= "0001";
 
-            if A = "00" then Y_L <= "0001"; 
-            elsif A = "01" then Y_L <= "0010";
-            elsif A = "10" then Y_L <= "0100";
-            elsif A = "11" then Y_L <= "1000";
-            else Y_L <= "0000";
-            end if;
+                when "01" =>
+                    Y_L <= "0010";
 
+                when "10" =>
+                    Y_L <= "0100";
+
+                when "11" =>
+                    Y_L <= "1000";
+                when others => 
+                    Y_L <= "0000"; 
+
+            end case;
+        else Y_L <= "0000"; 
         end if;
 
-    end process; 
-end behav_decoder_2to4en;
+    end process;
+
+end architecture behav_decoder_2to4en; 
---
+++ SEM3/sistemas-digitais/vhdl/mux4x1/mux4x1.vhd
@@ -0,0 +1,14 @@
+entity mux is
+    port (
+        I : in std_logic_vector (3 downto 0); 
+        SEL : in std_logic_vector (1 downto 0); 
+        Y : out std_logic 
+    );
+end entity mux;
+
+architecture behav_mux of mux is 
+begin 
+
+    
+
+end architecture behav_mux; 
