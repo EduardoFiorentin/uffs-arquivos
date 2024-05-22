40fb41df8388a7c0aaa7d8cfae06846fa65a8347
Eduardo Fiorentin <95770285+EduardoFiorentin@users.noreply.github.com>
2024-05-22 00:32:31 -0300

    adaptação t02 vhdl

--- SEM3/sistemas-digitais/T02/busArbiter copy.vhd
+++ SEM3/sistemas-digitais/T02/busArbiter copy.vhd
@@ -3,19 +3,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity busArbiter is
     Port (
-        KEY: in std_logic_vector(3 downto 0); 
-        sw: in std_logic_vector(9 downto 0);
-		ledR: out std_logic_vector(9 downto 0)
-        -- clk    : in STD_LOGIC;
-        -- reset  : in STD_LOGIC;
-        -- req0   : in STD_LOGIC;
-        -- req1   : in STD_LOGIC;
-        -- req2   : in STD_LOGIC;
-        -- req3   : in STD_LOGIC;
-        -- aut0   : out STD_LOGIC;
-        -- aut1   : out STD_LOGIC;
-        -- aut2   : out STD_LOGIC;
-        -- aut3   : out STD_LOGIC
+        clk    : in STD_LOGIC;
+        reset  : in STD_LOGIC;
+        req0   : in STD_LOGIC;
+        req1   : in STD_LOGIC;
+        req2   : in STD_LOGIC;
+        req3   : in STD_LOGIC;
+        aut0   : out STD_LOGIC;
+        aut1   : out STD_LOGIC;
+        aut2   : out STD_LOGIC;
+        aut3   : out STD_LOGIC
     );
 end busArbiter;
 
@@ -23,14 +20,14 @@ architecture behavBusArbiter of busArbiter is
     type state_type is (IDLE, PREF0, PREF1, PREF2, PREF3);
     signal state, next_state : state_type := IDLE;                  -- ?
 begin
-    process(KEY(0), sw(1))
 
     -- controle de saídas 
+    process(clk, reset)
     begin
-        if (sw(1) = '0') then -- alt
-        elsif (KEY(0)'EVENT and KEY(0) = '0') then  -- alt
+        if (reset = '1') then 
             state <= IDLE; 
         
+        elsif (clk'EVENT and clk = '1') then 
             state <= next_state; 
         
         end if; 
@@ -44,7 +41,7 @@ begin
         
         -- manter estado atual 
 
-    process(state, sw(2), sw(3), sw(4), sw(5)) 
+    process(state, req0, req1, req2, req3) 
     begin
         -- manter estado atual 
         next_state <= state;   
@@ -52,32 +49,32 @@ begin
 
             -- de IDLE para algum dos outros 
             when IDLE => 
-                if sw(2) = '0' then 
+                if req0 = '1' then 
                     next_state <= PREF0; 
-                elsif sw(3) = '0' then 
+                elsif req1 = '1' then 
                     next_state <= PREF1; 
-                elsif sw(4) = '0' then 
+                elsif req2 = '1' then 
                     next_state <= PREF2; 
-                elsif sw(5) = '0' then 
+                elsif req3 = '1' then 
                     next_state <= PREF3; 
                 else 
                     next_state <= IDLE; 
                 end if;
 
             when PREF0 => 
-                if sw(2) = '1' then
+                if req0 = '0' then
                     next_state <= IDLE;
                 end if;
             when PREF1 => 
-                if sw(3) = '1' then
+                if req1 = '0' then
                     next_state <= IDLE;
                 end if;
             when PREF2 => 
-                if sw(4) = '1' then
+                if req2 = '0' then
                     next_state <= IDLE;
                 end if;
             when PREF3 => 
-                if sw(5) = '1' then
+                if req3 = '0' then
                     next_state <= IDLE;
                 end if;
 
@@ -88,40 +85,40 @@ begin
 
     process(state)
     begin
-        ledR(0) <= '0';
-        ledR(1) <= '0';
-        ledR(2) <= '0';
-        ledR(3) <= '0';
+        aut0 <= '0';
+        aut1 <= '0';
+        aut2 <= '0';
+        aut3 <= '0';
         case state is
             -- when IDLE => 
-            --     ledR(0) <= '0';
-            --     ledR(1) <= '0';
-            --     ledR(2) <= '0';
-            --     ledR(3) <= '0';
+            --     aut0 <= '0';
+            --     aut1 <= '0';
+            --     aut2 <= '0';
+            --     aut3 <= '0';
 
             when PREF0 =>
-                ledR(0) <= '1';
-                -- ledR(1) <= '0';
-                -- ledR(2) <= '0';
-                -- ledR(3) <= '0';
+                aut0 <= '1';
+                -- aut1 <= '0';
+                -- aut2 <= '0';
+                -- aut3 <= '0';
 
             when PREF1 =>
-                ledR(1) <= '1';
-                -- ledR(0) <= '0';
-                -- ledR(2) <= '0';
-                -- ledR(3) <= '0';
+                aut1 <= '1';
+                -- aut0 <= '0';
+                -- aut2 <= '0';
+                -- aut3 <= '0';
 
             when PREF2 =>
-                ledR(2) <= '1';
-                -- ledR(0) <= '0';
-                -- ledR(1) <= '0';
-                -- ledR(3) <= '0';
+                aut2 <= '1';
+                -- aut0 <= '0';
+                -- aut1 <= '0';
+                -- aut3 <= '0';
 
             when PREF3 =>
-                ledR(3) <= '1';
-                -- ledR(0) <= '0';
-                -- ledR(1) <= '0';
-                -- ledR(2) <= '0';
+                aut3 <= '1';
+                -- aut0 <= '0';
+                -- aut1 <= '0';
+                -- aut2 <= '0';
             when others =>
                 null;
         end case;
--- SEM3/sistemas-digitais/T02/codigofpga.vhd
+++ SEM3/sistemas-digitais/T02/codigofpga.vhd
@@ -5,14 +5,15 @@ entity busArbiter is
     Port (
         KEY: in std_logic_vector(3 downto 0); 
         sw: in std_logic_vector(9 downto 0);
-		ledR: out std_logic_vector(9 downto 0)
+		  ledR: out std_logic_vector(9 downto 0)
         -- clk    : in STD_LOGIC;
         -- reset  : in STD_LOGIC;
-        -- req0   : in STD_LOGIC;
+        -- req	   : in STD_LOGIC;
         -- req1   : in STD_LOGIC;
         -- req2   : in STD_LOGIC;
         -- req3   : in STD_LOGIC;
-        -- aut0   : out STD_LOGIC;
+		  
+        -- aut	   : out STD_LOGIC;
         -- aut1   : out STD_LOGIC;
         -- aut2   : out STD_LOGIC;
         -- aut3   : out STD_LOGIC
@@ -27,7 +28,9 @@ begin
 
     -- controle de saídas 
     begin
-        if (sw(1) = '0') then -- alt
+        if (sw(1) = '1') then -- alt
+				state <= IDLE; 
+				
         elsif (KEY(0)'EVENT and KEY(0) = '0') then  -- alt
             state <= IDLE; 
         
@@ -52,32 +55,32 @@ begin
 
             -- de IDLE para algum dos outros 
             when IDLE => 
-                if sw(2) = '0' then 
+                if sw(2) = '1' then 
                     next_state <= PREF0; 
-                elsif sw(3) = '0' then 
+                elsif sw(3) = '1' then 
                     next_state <= PREF1; 
-                elsif sw(4) = '0' then 
+                elsif sw(4) = '1' then 
                     next_state <= PREF2; 
-                elsif sw(5) = '0' then 
+                elsif sw(5) = '1' then 
                     next_state <= PREF3; 
                 else 
                     next_state <= IDLE; 
                 end if;
 
             when PREF0 => 
-                if sw(2) = '1' then
+                if sw(2) = '0' then
                     next_state <= IDLE;
                 end if;
             when PREF1 => 
-                if sw(3) = '1' then
+                if sw(3) = '0' then
                     next_state <= IDLE;
                 end if;
             when PREF2 => 
-                if sw(4) = '1' then
+                if sw(4) = '0' then
                     next_state <= IDLE;
                 end if;
             when PREF3 => 
-                if sw(5) = '1' then
+                if sw(5) = '0' then
                     next_state <= IDLE;
                 end if;
 
@@ -88,43 +91,42 @@ begin
 
     process(state)
     begin
-        ledR(0) <= '0';
-        ledR(1) <= '0';
-        ledR(2) <= '0';
-        ledR(3) <= '0';
+        --ledR(1) <= '	';
+        --ledR(2) <= '	';
+        --ledR(3) <= '	';
+        --ledR(4) <= '	';
         case state is
-            -- when IDLE => 
-            --     ledR(0) <= '0';
-            --     ledR(1) <= '0';
-            --     ledR(2) <= '0';
-            --     ledR(3) <= '0';
+             when IDLE => 
+                 ledR(2) <= '0';
+                 ledR(3) <= '0';
+                 ledR(4) <= '0';
+                 ledR(5) <= '0';
 
             when PREF0 =>
-                ledR(0) <= '1';
-                -- ledR(1) <= '0';
-                -- ledR(2) <= '0';
-                -- ledR(3) <= '0';
+                ledR(2) <= '1';
+                ledR(3) <= '0';
+                ledR(4) <= '0';
+                ledR(5) <= '0';
 
             when PREF1 =>
-                ledR(1) <= '1';
-                -- ledR(0) <= '0';
-                -- ledR(2) <= '0';
-                -- ledR(3) <= '0';
+                ledR(3) <= '1';
+                ledR(2) <= '0';
+                ledR(4) <= '0';
+                ledR(5) <= '0';
 
             when PREF2 =>
-                ledR(2) <= '1';
-                -- ledR(0) <= '0';
-                -- ledR(1) <= '0';
-                -- ledR(3) <= '0';
+                ledR(4) <= '1';
+                ledR(2) <= '0';
+                ledR(3) <= '0';
+                ledR(5) <= '0';
 
             when PREF3 =>
-                ledR(3) <= '1';
-                -- ledR(0) <= '0';
-                -- ledR(1) <= '0';
-                -- ledR(2) <= '0';
+                ledR(5) <= '1';
+                ledR(2) <= '0';
+                ledR(3) <= '0';
+                ledR(4) <= '0';
             when others =>
                 null;
         end case;
     end process;
-
 end architecture behavBusArbiter; 
