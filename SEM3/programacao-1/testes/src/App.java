f03cdf3408df3b028ee8069a326412966069afb3
Eduardo Fiorentin <95770285+EduardoFiorentin@users.noreply.github.com>
2024-06-04 17:32:11 -0300

    arquivos de teste java

---
+++ SEM3/programacao-1/testes/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in); 
+
+        String teste = scanner.nextLine();
+
+        System.out.println(teste.equals(""));
+    }
+}
